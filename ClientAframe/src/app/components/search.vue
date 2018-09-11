ce2a5e2f872c48febca5f364c5a1ebc134d53141
Yikai Yang <yikai.yang@outlook.com>
2018-09-11 04:52:24 +0200

    playing around with vue.js

---
+++ ClientAframe/src/app/components/search.vue
@@ -0,0 +1,139 @@
+<template>
+      <div>
+            <form id="searchbar">
+                <div class="search-container">
+                    <button id="menu" type="button" onclick="openMenu()"></button>
+                    <input id="textinput" type="text" name="searchbox" placeholder="Search location">
+                </div>
+            </form>
+            
+
+            <div id="search-results">
+                <ul>
+                    <li>
+                        <span class="result-name">Vienna</span>
+                        <span class="delimiter">•</span>
+                        <span class="result-city">Austria</span>
+                    </li>
+                    <li>
+                        <span class="result-name">Vienna</span> 
+                        <span class="delimiter">•</span>
+                        <span class="result-city">Austria</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'search'
+}
+</script>
+
+<style>
+    form {
+        background: #FFFFFF;
+        box-shadow: 0 2px 2px 0 rgba(169,169,169,0.50);
+        border-radius: 2px;
+
+        width: 340px;
+        height: 48px;
+        
+        margin-top: 18px;
+        margin-left: 18px;
+        cursor: default;
+    }
+
+    .search-container {
+        display: flex;
+    }
+
+    #searchbar {
+        background-color: white;
+    }
+
+    #searchbar #menu{
+        border: 0px;
+        height: 48px;
+        width: 48px;
+        background-color: white;
+        background-image: url('../img/icons/menu.svg');
+        background-repeat: no-repeat;
+        background-size: 16px;
+        background-position: center;
+        cursor: pointer;
+    }
+
+    #searchbar #textinput{
+        border: none;
+    
+        margin-left: 14px;
+        border: 0;
+        padding: 0;
+        height: 48px;
+        font-size: 14px;
+        flex-grow: 2;   
+        font-size: 14px;
+        color: #616569;
+        outline: none;
+    }
+
+    #search-results {
+        background: #FFFFFF;
+        box-shadow: 0 2px 2px 0 rgba(169,169,169,0.50);
+        border-radius: 2px;
+        margin-left: 18px;
+        margin-top: 8px;
+        cursor: default;
+    }
+
+    #search-results li {
+        margin: 0;
+        padding: 0px;
+        height: 42px;
+        border-bottom: 1px solid #EDEDED;
+        cursor: pointer;
+    }
+
+    #search-results li:last-child{
+        border-bottom: 0px;
+    }
+
+    #search-results li:hover {
+        background-color: #F4F4F4;
+    }
+
+    #search-results li span {
+        height: 42px;
+        display: inline-flex;
+        align-items: center;
+        font-size: 14px;
+    }
+
+    #search-results .result-name{
+        color: #2D2D2D;
+        margin-right: 4px;
+    }
+
+    .delimiter {
+        color: #D8D8D8;
+    }
+
+    #search-results .result-city{
+        color: #878787;
+        margin-left: 4px;
+    }
+
+    #search-results ul {
+        /** Remove bootstrap padding*/
+        margin-left: 0;
+        padding-left: 0;
+
+        list-style: none;
+    }
+
+    #search-results .result-name {
+        margin-left: 18px;
+    }
+</style>
