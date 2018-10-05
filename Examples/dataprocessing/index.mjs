0e11607f2717462462571d5144b4cb9714bf40c6
Yikai Yang <yikai.yang@outlook.com>
2018-10-05 03:42:52 +0200

    converted csv to json

--- Examples/dataprocessing/index.mjs
+++ Examples/dataprocessing/index.mjs
@@ -1,9 +1,15 @@
 import DataManager from './datamanager.mjs';
 import d3 from 'd3';
-import fetchPolyFill from 'node-fetch-polyfill';
 import path from 'path';
-import fetch from 'node-fetch';
+//import fetch from 'node-fetch';
+import fetchPolyfill from 'node-fetch-polyfill';
 import express from 'express';
+import papaparse from 'papaparse'
+import fs from 'fs';
+
+if (typeof fetch !== 'function'){
+    global.fetch = fetchPolyfill;
+}
 
 /**
  * Express setup
@@ -20,27 +26,32 @@ let server = app.listen(port, function(){
 app.get('/', (req, res) => res.send('Working Server'));
 app.use(express.static('public'));
 
-const __dirname = path.resolve(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
+///End express setup
 
-/* if (typeof fetch !== 'function'){
-    global.fetch = fetchPolyFill;
-} */
+const __dirname = path.resolve(path.dirname(decodeURI(new URL(import.meta.url).pathname)));
 
 const dirPath = './assets/data/hospital';
 const files = [
     'BetterHosp.csv',
     'HospitalData.csv',
-    'lookup_address.json'
+    //'lookup_address.json'
 ];
 
+
+const serverURL = 'http://localhost:8081';
+
 let filePaths = [];
+let serverFilePaths = [];
+
 for (let i = 0; i < files.length; i++){
     filePaths.push(dirPath + '/' + files[i]);
+    serverFilePaths.push(serverURL + '/assets/data/hospital/' + files[i]);
 }
 
 filePaths = filePaths.map(p => __dirname + '/' + path.normalize(p));
 
 console.log(filePaths);
+console.log(serverFilePaths);
 let data = [];
 
 /* for (let i = 0; i < filePaths.length; i++){
@@ -58,16 +69,63 @@ let data = [];
     }
 } */
 //console.log(path.resolve('haltestellen.csv'));
-const haltestellenPath = path.resolve('haltestellen.csv');
 
-fetch('http://localhost:8081/assets/data/haltestellen.csv').then((response) => {
-    console.log(response);
+const serverHostedPath = 'http://localhost:8081/assets/data/haltestellen.csv';
+
+/* fetch('http://localhost:8081/assets/data/haltestellen.csv').then((response) => {
+    response.text().then(
+        (data) => {
+            console.log(data);
+        }
+    )
+}); */
+
+
+/**
+ * Converts a csv file to a json file.
+ * @param originFilePath 
+ * @param destinationPath 
+ */
+function convertCSVtoJSONFile(originFilePath, destinationPath){
+    fetch(originFilePath).then((response) => {
+        response.text().then(
+            (data) => {
+                //console.log(data);
+                parseCSVData(data, destinationPath);
+            }
+        )
+    });
+}
+
+function parseCSVData(data, destinationPath = 'data.json'){
+    const result = papaparse.parse(data, {
+        header: true,
+        complete: (result) => {
+            console.log('complete');
+            const jsonData = JSON.stringify(result);
+            fs.writeFile(destinationPath, jsonData, (error) => {
+                if (error){
+                    console.error('File could not be written: ' + error);
+                }
+            });
+        }
+    });
+}
+
+//convertCSVtoJSONFile(serverHostedPath);
+
+for (let i=0; i < serverFilePaths.length; i++){
+    convertCSVtoJSONFile(serverFilePaths[i], files[i].split('.')[0] + '.json');
+}
+
+
+/* d3.dsv(';', serverHostedPath, (data) => {
+    //console.log(data);
+    let dataString = JSON.stringify(data);
+    fs.writeFile('data.json', dataString, (error) => {
+        if (error){
+            console.error('error: ' + error);
+        }
+    });
 });
-
-
-/* d3.csv(path.resolve('haltestellen.csv'), (data) => {
-    console.log(data);
-});  */
-
-console.log(data);
-
+ */
